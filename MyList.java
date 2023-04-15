92b750b7444576d793255d4e6d085002268c30c9
Sabina <sabina.kass05@icloud.com>
2023-04-16 00:52:00 +0600

    Added some new methods

--- MyList.java
+++ MyList.java
@@ -1,5 +1,13 @@
 public interface MyList<T> extends Iterable<T> {
     void add(T item);
+    void add(T item, int index);
+    boolean remove(T item);
+    T remove(int index);
     int size();
+    void clear();
+    boolean contains(Object o);
     T get(int index);
+    int indexOf(Object o);
+    int lastIndexOf(Object o);
+    void sort();
 }
